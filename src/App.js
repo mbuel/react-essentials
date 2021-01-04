a68ae2b7c50bcf6e86be073a50536dda505762dd
Morris Buel <morris.buel@gmail.com>
2021-01-03 17:19:20 -0800

    End of Sec 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import restaurant from './restaurant.jpg'
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name} Cacino</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve {props.adjective} delicacies daily.</p>
+      <img 
+        src={restaurant} 
+        height={200} 
+        alt="outside a restaurant"/>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          // eslint-disable-next-line react/jsx-key
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Eggs",
+  "Challah",
+  "French Toast"
+]
+
+const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Nezmah"/>
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
 ReactDOM.render(
-  <ul>
-    <li>Day 1</li>
-    <li>Day 2</li>
-    <li>Day 3</li>
-    <li>Day 4</li>
-  </ul>,
+  <App />,
   document.getElementById('root')
 );
